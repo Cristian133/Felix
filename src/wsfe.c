2d89575c6c3eb5878f7beda078174765f746ad2c
Cristian133 <cristian.andrione@gmail.com>
2020-12-25 11:08:02 -0300

    Se borran comentarios innecesarios

--- src/wsfe.c
+++ src/wsfe.c
@@ -2,20 +2,6 @@
  * Metodo encargado de recibir los argumentos que conforman un comprobante
  * y pedir CAE a AFIP.
  *
- * Copyright (C) 2019  Cristian Andrione.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "ServiceSoap.nsmap"
